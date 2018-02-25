96a5561e9d1e0cd4ebfb4e0f4bd32bea93a46569
eggobiggie <miacrandell@gmail.com>
2018-02-25 15:07:45 -0700

    connection changes

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,27 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-    port: 8889,
-    host: "localhost",
-    user: "root",
-    password: "root",
-    database: "burger_db"
-});
+// var connection = mysql.createConnection({
+//     port: 8889,
+//     host: "localhost",
+//     user: "root",
+//     password: "root",
+//     database: "burger_db"
+// });
+
+var connection;
+if(process.env.JAWSDB_URL) {
+  //Heroku deployment
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  //local host
+    connection = mysql.createConnection({
+        root: 3000,
+        host: "localhost",
+        user: "root",
+        password: "",
+        database: "db_name",
+    });
+};
 
 connection.connect(function(err) {
     if (err) throw err;
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var bodyParser = require('body-parser');
 
 var app = express();
 
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 3000;
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: false }));
